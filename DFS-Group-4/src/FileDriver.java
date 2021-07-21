16b8b6fc07879be8f09f72978db2cb5198b4bdee
Martin Le <62312380+madmartian8@users.noreply.github.com>
2021-07-21 01:37:28 -0700

    attempt to add newly locally created files

---
+++ DFS-Group-4/src/FileDriver.java
@@ -0,0 +1,11 @@
+
+public class FileDriver {
+	
+	public static void main(String[] args) {
+		
+		//stub
+
+		
+	}
+	
+}
---
+++ DFS-Group-4/src/FileManager.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class FileManager {
+
+	private int numFiles;
+	
+	private File[] filesArray;
+	
+	public FileManager() {
+		numFiles = 0;
+		filesArray = new File[0];
+	}
+	
+	public String toString() {
+		// Return a string containing all the files in the
+		// order they are stored in the array along with
+		// the values for numFiles and the length of the array.
+		String s = "numFiles=" + numFiles + "\nfileArray.length = " + filesArray.length + "\n";
+	       for (int i = 0; i < numFiles; i++) {
+	           s += "fileArray[" + i + "] = " + filesArray[i] + "\n";
+	       }
+	       return s;
+	}
+
+
+
+
+
+	
+}
